18b3c0e39c96195685cd43d57bc2010e4e55c58a
Cabral <jcabral304@gmail.com>
2022-03-10 15:32:40 -0300

    [component Home.js App.js-Route] feito

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ import React from 'react';
 class Home extends React.Component {
   render() {
     return (
-      <h1>Home</h1>
+      <h2
+        data-testid="home-initial-message"
+      >
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </h2>
     );
   }
 }
