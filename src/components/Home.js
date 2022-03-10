76a3f57b4e48ea41478e831952f1c825aa4eccc3
Thiago Henrique <thiagoedusan5.11@outlook.com>
2022-03-10 17:07:45 -0300

    Requisito 4 OK

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getCategoriesFromApi();
+  }
+
+  getCategoriesFromApi = async () => {
+    const response = await getCategories();
+    this.setState({ categories: [...response] });
+  }
+
   render() {
+    const {
+      categories,
+    } = this.state;
+
     return (
-      <h2
-        data-testid="home-initial-message"
-      >
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </h2>
+      <>
+        <h2
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h2>
+        {categories.map((categorie) => (
+          <button
+            data-testid="category"
+            type="button"
+            key={ categorie.name }
+          >
+            { categorie.name }
+          </button>
+        )) }
+      </>
     );
   }
 }
